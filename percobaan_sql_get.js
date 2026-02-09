dc7a98cee457db8df01f83d977dd7d4567bae766
HanGieTime <elgykputra155@gmail.com>
2026-02-09 22:09:58 +0700

    konek database

---
+++ percobaan_sql_get.js
@@ -0,0 +1,32 @@
+// require punya 3 fungsi
+// 1. memanggil modul/fitur bawaan node.js
+// 2. memanggil modul/fitur dari npmjs
+// 3. memanggil file lain
+
+// peanggil modul mysql2 dari node_modules
+const mysql = require('mysql2')
+
+// cara menghubungkan dengan database
+const database = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'',
+  database:'javascript_fullstack_januari2026'
+})
+database.connect()
+
+try{
+  database.query('SELECT * FROM karyawan', function (error, hasil) {
+    if (error){
+      throw error.message()
+    }
+    let semuaNama = ''
+    for (const i in hasil) {
+      semuaNama += hasil[i].nama + '\n'
+    }
+    console.log(semuaNama)
+  })
+  database.end()
+}catch (error){
+console.log(error)
+}
